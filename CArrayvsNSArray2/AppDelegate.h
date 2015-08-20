e1a6bbcdf6a7ef9429c00a132c4b356671b53dfb
Do Minh Hai <dominhhai@MacBook-Air.local>
2015-08-20 18:13:03 +0700

    Initial Commit

---
+++ CArrayvsNSArray2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CArrayvsNSArray2
+//
+//  Created by Do Minh Hai on 8/20/15.
+//  Copyright (c) 2015 Do Minh Hai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
